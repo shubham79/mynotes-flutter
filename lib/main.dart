d0f8d543e87caa1e15bc8bfa19dc3732530f5e04
Shubham Agarwal <shubhamrocks0709@gmail.com>
2022-10-29 01:25:31 +0530

    step-9 Replace firebase with Authservice in UI views

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,10 @@
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:mynotes/constants/routes.dart';
+import 'package:mynotes/services/auth/auth_service.dart';
 import 'package:mynotes/views/login_view.dart';
+import 'package:mynotes/views/notes_view.dart';
 import 'package:mynotes/views/register_view.dart';
 import 'package:mynotes/views/verify_email_view.dart';
-import 'firebase_options.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -31,17 +30,15 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return FutureBuilder(
-        future: Firebase.initializeApp(
-          options: DefaultFirebaseOptions.currentPlatform,
-        ),
+        future: AuthService.firebase().initialize(),
         builder: (context, snapshot) {
           switch (snapshot.connectionState) {
             case ConnectionState.done:
-              final user = (FirebaseAuth.instance.currentUser);
+              final user = AuthService.firebase().currentUser;
               if (user == null) {
                 return const LoginView();
               }
-              final emailVerfied = user.emailVerified;
+              final emailVerfied = user.isEmailVerified;
               if (emailVerfied) {
                 return const NotesView();
               } else {
@@ -54,68 +51,4 @@ class HomePage extends StatelessWidget {
           }
         });
   }
-}
-
-enum MenuAction { logout }
-
-class NotesView extends StatefulWidget {
-  const NotesView({super.key});
-
-  @override
-  State<NotesView> createState() => _NotesViewState();
-}
-
-class _NotesViewState extends State<NotesView> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Main UI'),
-        actions: [
-          PopupMenuButton<MenuAction>(onSelected: (value) async {
-            switch (value) {
-              case MenuAction.logout:
-                final shouldLogout = await showLogOutDialog(context);
-                if (shouldLogout) {
-                  // Log out from Firebase
-                  await FirebaseAuth.instance.signOut();
-                  Navigator.of(context)
-                      .pushNamedAndRemoveUntil(loginRoute, (_) => false);
-                }
-                break;
-            }
-          }, itemBuilder: ((context) {
-            return [
-              const PopupMenuItem<MenuAction>(
-                  value: MenuAction.logout, child: Text('Logout'))
-            ];
-          }))
-        ],
-      ),
-      body: const Text('Notes'),
-    );
-  }
-}
-
-Future<bool> showLogOutDialog(BuildContext context) {
-  return showDialog<bool>(
-      context: context,
-      builder: (context) {
-        return AlertDialog(
-          title: const Text('Sign Out'),
-          content: const Text('Are you sure you want to logout?'),
-          actions: [
-            TextButton(
-                onPressed: (() {
-                  Navigator.of(context).pop(false);
-                }),
-                child: const Text('Cancel')),
-            TextButton(
-                onPressed: (() {
-                  Navigator.of(context).pop(true);
-                }),
-                child: const Text('Log out'))
-          ],
-        );
-      }).then((value) => value ?? false);
 }
--- lib/views/login_view.dart
+++ lib/views/login_view.dart
@@ -1,6 +1,7 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:mynotes/constants/routes.dart';
+import 'package:mynotes/services/auth/auth_exceptions.dart';
+import 'package:mynotes/services/auth/auth_service.dart';
 import 'package:mynotes/utilities/show_error_dialog.dart';
 
 class LoginView extends StatefulWidget {
@@ -53,10 +54,10 @@ class _LoginViewState extends State<LoginView> {
                 final email = _email.text;
                 final password = _password.text;
                 try {
-                  await FirebaseAuth.instance.signInWithEmailAndPassword(
-                      email: email, password: password);
-                  final user = FirebaseAuth.instance.currentUser;
-                  final userVerified = user?.emailVerified ?? false;
+                  await AuthService.firebase()
+                      .logIn(email: email, password: password);
+                  final user = AuthService.firebase().currentUser;
+                  final userVerified = user?.isEmailVerified ?? false;
                   if (userVerified) {
                     Navigator.of(context).pushNamedAndRemoveUntil(
                       notesRoute,
@@ -69,16 +70,12 @@ class _LoginViewState extends State<LoginView> {
                       (route) => false,
                     );
                   }
-                } on FirebaseAuthException catch (e) {
-                  if (e.code == 'user-not-found') {
-                    await showErrorDialog(context, 'User not found');
-                  } else if (e.code == 'wrong-password') {
-                    await showErrorDialog(context, 'Wrong Password');
-                  } else {
-                    await showErrorDialog(context, 'Error: ${e.code}');
-                  }
-                } catch (e) {
-                  await showErrorDialog(context, 'Error: ${e.toString()}');
+                } on UserNotFoundAuthException {
+                  await showErrorDialog(context, 'User not found!');
+                } on WrongPasswordAuthException {
+                  await showErrorDialog(context, 'Wrong Password');
+                } on GenericAuthException {
+                  await showErrorDialog(context, 'Authentication error');
                 }
               }),
               child: const Text('Login')),
