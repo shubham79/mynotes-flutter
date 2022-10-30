1042b74d1bd9f10dc0c960657813f16f5e8006cb
Shubham Agarwal <shubhamrocks0709@gmail.com>
2022-10-31 02:22:35 +0530

    step-17 Update existing Note completed

--- lib/views/notes/notes_view.dart
+++ lib/views/notes/notes_view.dart
@@ -32,7 +32,7 @@ class _NotesViewState extends State<NotesView> {
         actions: [
           IconButton(
             onPressed: (() {
-              Navigator.of(context).pushNamed(newNoteRoute);
+              Navigator.of(context).pushNamed(createOrUpdateNewNoteRoute);
             }),
             icon: const Icon(Icons.add),
           ),
@@ -70,10 +70,17 @@ class _NotesViewState extends State<NotesView> {
                         if (snapshot.hasData) {
                           final allNotes = snapshot.data!;
                           return NotesListView(
-                              notes: allNotes,
-                              onDeleteNote: (note) async {
-                                await _notesService.deleteNote(id: note.id);
-                              });
+                            notes: allNotes,
+                            onDeleteNote: (note) async {
+                              await _notesService.deleteNote(id: note.id);
+                            },
+                            onTap: (note) {
+                              Navigator.of(context).pushNamed(
+                                createOrUpdateNewNoteRoute,
+                                arguments: note,
+                              );
+                            },
+                          );
                         } else {
                           return const CircularProgressIndicator();
                         }
